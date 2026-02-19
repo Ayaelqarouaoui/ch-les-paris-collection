8bdbeb2cfb257b23c78b4dda8d346f1c18fee990
Ayaelqarouaoui <ayaelqarouaoui@gmail.com>
2026-02-19 21:20:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- base: '/ch-les-paris-collection/',
+  // Hadi hiya l-asasiya bach t-hl 404
+  base: '/ch-les-paris-collection/', 
   server: {
     host: "::",
     port: 8080,
